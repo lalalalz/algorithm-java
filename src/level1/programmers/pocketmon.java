1d30987fad05c3a77bd3405ce1d446c19fef7422
lalalalz <lalalalz@naver.com>
2022-07-04 22:11:15 +0900

    #ADD 1. 예산 2. 숫자 골라 더하기 3. 폰켓몬

---
+++ src/level1/programmers/pocketmon.java
@@ -0,0 +1,29 @@
+package level1.programmers;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class pocketmon {
+
+    public int solution(int[] nums) {
+        int answer = 0;
+
+        int pocketMons = nums.length;
+        Set<Integer> pocketMonTypes = new HashSet<>();
+
+        for (int num : nums) {
+            pocketMonTypes.add(num);
+        }
+
+        if (pocketMonTypes.size() < pocketMons / 2) {
+            answer = pocketMonTypes.size();
+        }
+        else {
+            answer = pocketMons / 2;
+        }
+
+        return answer;
+    }
+
+}
