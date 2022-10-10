f4ebe356f691c9bbf2836d92587fec9439fc1ed5
lalalalz <lalalalz@naver.com>
2022-10-10 23:09:27 +0900

    #ADD 1. SearchInsertPosition

---
+++ src/leetCode/SearchInsertPosition.java
@@ -0,0 +1,12 @@
+package leetCode;
+
+public class SearchInsertPosition {
+
+    public int searchInsert(int[] nums, int target) {
+        return getFindIndexOf(nums, target);
+    }
+
+    private int getFindIndexOf(int[] nums, int target) {
+        return 0;
+    }
+}
