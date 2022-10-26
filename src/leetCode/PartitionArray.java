3fdb23378313eac2c8234588bdc67f5c21c1b41d
lalalalz <lalalalz@naver.com>
2022-10-26 19:09:26 +0900

    #ADD 1. Partition Array

--- src/leetCode/PartitionArray.java
+++ src/leetCode/PartitionArray.java
@@ -1,8 +1,37 @@
 package leetCode;
 
-public class PartitionArray {
-
+class PartitionArray {
     public boolean canThreePartsEqualSum(int[] arr) {
-        return true;
+        long sum = getSumOfArray(arr);
+        return doThreeParts(arr, sum);
+    }
+
+    private long getSumOfArray(int[] arr) {
+        long sum = 0;
+
+        for (int number : arr) {
+            sum += number;
+        }
+
+        return sum;
+    }
+
+    private boolean doThreeParts(int[] arr, long sum) {
+        int index = 0;
+        long sumOfParts = 0;
+        long oneThird = sum / 3;
+
+        if(sum % 3 > 0) return false;
+
+        for (int number : arr) {
+            sumOfParts += number;
+
+            if (index < 3 && sumOfParts == oneThird) {
+                sumOfParts = 0;
+                index++;
+            }
+        }
+
+        return index == 3 && sumOfParts == 0;
     }
 }
