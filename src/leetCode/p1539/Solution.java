6db33d78592225899952eeac28a9a929ea158b3f
lalalalz <lalalalz@naver.com>
2026-01-23 17:52:22 +0900

    23

--- src/leetCode/p1539/Solution.java
+++ src/leetCode/p1539/Solution.java
@@ -1,7 +1,28 @@
 package leetCode.p1539;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
     public int findKthPositive(int[] arr, int k) {
+        int answer = 0;
+        int exclusiveNumberCount = 0;
+        Set<Integer> inclusive = new HashSet<>();
 
+        for (int number : arr) {
+            inclusive.add(number);
+        }
+
+        for (int i = 1; ; i++) {
+            if (!inclusive.contains(i)) {
+                exclusiveNumberCount++;
+            }
+            if (exclusiveNumberCount == k) {
+                answer = i;
+                break;
+            }
+        }
+
+        return answer;
     }
 }
